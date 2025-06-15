e46b9f67b5dc1be8a9b09a0d7615df7109443861
yassine azedine <yassine.azedine0711@gmail.com>
2025-06-15 12:46:20 +0100

    first commut

---
+++ test/index.text.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { add } = require('../index');
+
+describe('Fonction add', () => {
+  it('devrait retourner 4 pour add(1, 3)', () => {
+    assert.strictEqual(add(1, 3), 4);
+  });
+
+  it('devrait retourner 0 pour add(-2, 2)', () => {
+    assert.strictEqual(add(-2, 2), 0);
+  });
+});
