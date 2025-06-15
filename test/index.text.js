eedb4486ff8274014ea78e8b313260f5521abc1b
yassine azedine <yassine.azedine0711@gmail.com>
2025-06-15 12:49:48 +0100

    test echoue

--- test/index.text.js
+++ test/index.text.js
@@ -3,7 +3,7 @@ const { add } = require('../index');
 
 describe('Fonction add', () => {
   it('devrait retourner 4 pour add(1, 3)', () => {
-    assert.strictEqual(add(1, 3), 4);
+    assert.strictEqual(add(1, 7), 4);
   });
 
   it('devrait retourner 0 pour add(-2, 2)', () => {
